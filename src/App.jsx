952e636d053b29469019cec4e500ea06eb04a765
asmraihan <asmraihanctg14@gmail.com>
2023-04-08 19:15:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="App">
+     <h2 className='text-4xl text-center p-5 text-blue-500'>Vite+Tailwind_Starter</h2>
+    </div>
+  )
+}
+
+export default App
